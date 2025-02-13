bc38d382ae74a3c6a5ffba8929af9a0eceab5d45
costaki <cskevofilax@utexas.edu>
2025-02-13 11:09:15 -0600

    fixed run_EQCCT_mseed in eqcctpro.py

--- eqcctpro.py
+++ eqcctpro.py
@@ -1,6 +1,5 @@
 
 import os 
-import psutil 
 
 from predictor import run_EQCCT_mseed, evaluate_system, find_optimal_configuration_cpu, find_optimal_configuration_gpu
 
@@ -11,19 +10,18 @@ csv_filepath = '/home/skevofilaxc/eqcctpro/csv'
 
 # Can run EQCCT on a given input dir on GPU or CPU 
 # Can also specify the number of stations you want to use as well  
-# run_EQCCT_mseed(use_gpu=True, 
-#                 intra_threads=1, 
-#                 inter_threads=1, 
-#                 ray_cpus=5,
-#                 input_dir=input_mseed_directory_path, 
-#                 output_dir=output_pick_directory_path, 
-#                 log_filepath=log_file_path, 
-#                 P_threshold=0.001, 
-#                 S_threshold=0.02, 
-#                 p_model_filepath='/home/skevofilaxc/model/ModelPS/test_trainer_024.h5', 
-#                 s_model_filepath='/home/skevofilaxc/model/ModelPS/test_trainer_021.h5', 
-#                 number_of_concurrent_predictions=5,
-#                 csv_dir=csv_filepath)
+run_EQCCT_mseed(use_gpu=True, 
+                intra_threads=1, 
+                inter_threads=1, 
+                ray_cpus=5,
+                input_dir=input_mseed_directory_path, 
+                output_dir=output_pick_directory_path, 
+                log_filepath=log_file_path, 
+                P_threshold=0.001, 
+                S_threshold=0.02, 
+                p_model_filepath='/home/skevofilaxc/model/ModelPS/test_trainer_024.h5', 
+                s_model_filepath='/home/skevofilaxc/model/ModelPS/test_trainer_021.h5', 
+                number_of_concurrent_predictions=5)
 
 # Can evalue your system by using the test input directory
 # Can either evaluate your system on the entire input dir or on a specific number of stations you want to evaluate 
